0f9792889891cd756f26d93cb28f569b4625b6fb
djey <djey9538@gmail.com>
2014-11-03 21:16:40 +0900

    root-url => 'hi django' test

--- puredj/urls.py
+++ puredj/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.http import HttpResponse
 
 urlpatterns = patterns('',
+	url(r'^$', lambda _ : HttpResponse('hi django')),
     # Examples:
     # url(r'^$', 'puredj.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
